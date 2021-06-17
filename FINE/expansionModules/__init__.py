f0dd3102a288e3784a5b80d03cebcfe7f8838f75
Julian Schönau <60855819+OfficialCodexplosive@users.noreply.github.com>
2021-06-17 10:50:23 +0200

    Add files via upload

---
+++ FINE/expansionModules/__init__.py
@@ -0,0 +1,8 @@
+"""
+Last edited: February 06, 2020
+|br| @author: FINE Developer Team (FZJ IEK-3)
+"""
+
+from .transformationPath import *
+from .robustPipelineSizing import *
+from .optimizeTSAmultiStage import *
