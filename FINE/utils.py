27b41dfd45828bf8632d0fbda3ac37e35ad0fe62
Julian Schönau <60855819+OfficialCodexplosive@users.noreply.github.com>
2021-06-18 14:07:05 +0200

    Update utils.py

--- FINE/utils.py
+++ FINE/utils.py
@@ -106,9 +106,9 @@ def checkTimeSeriesIndex(esM, data):
     Necessary if the data rows represent the time-dependent data:
     Check if the row-indices of the data match the time indices of the energy system model.
     """
+    str0 = ''.join(str(e)+", " for e in list(data.index))
+    str1 = ''.join(str(e)+", " for e in esM.totalTimeSteps)
     if list(data.index) != esM.totalTimeSteps:
-        str0 = ''.join(str(e)+", " for e in list(data.index))
-        str1 = ''.join(str(e)+", " for e in esM.totalTimeSteps)
         raise ValueError(str0 + ' / ' + str1 + 'Time indices do not match the one of the specified energy system model.')
 
 
